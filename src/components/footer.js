90dcc58b86e70cc905545d7ad5a2e96d4f391e4b
Rohit Goyal <rohit@unlu.io>
2021-10-23 20:35:39 +0530

    change user validation features

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,23 +1,33 @@
-import { NavLink } from "react-router-dom";
 import styles from '../App.module.scss'
 
 export default function Footer() {
   return (
-    <div>
-      <div className={styles.pageFooter}>
-        <NavLink
-          to="/"
-          className={styles.pageSwitcherItem}
-        >
-          SignIn
-        </NavLink>
-        <NavLink
-          to="/contact"
-          className={styles.pageSwitcherItem}
-        >
-          Contact Us
-        </NavLink>
-      </div>
+    <footer className={styles.pageFooter}>
+
+    <div className={styles.footerLeft}>
+
+      <h3 className={styles.footerCompanyName}>Company Name</h3>
+
+      <p className={styles.footerLinks}>
+        <a href="/">Home</a>
+        <a href="/about-us">About Us</a>
+        <a href="/contact">Contact Us</a>
+      </p>
     </div>
-  );
+
+    <div className={styles.footerCenter}>
+      <h3 className={styles.footerCompanyName}>Address:</h3>
+      <p className={styles.footerInfo}>444 City CentreTech Park, New Delhi</p>
+    </div>
+
+    <div className={styles.footerRight}>
+
+    <h3 className={styles.footerCompanyName}>About the company</h3>
+
+      <p className={styles.footerInfo}>
+        Lorem ipsum dolor sit amet, consectateur.
+      </p>
+    </div>
+  </footer>
+  )
 }
--- src/components/user-form.js
+++ src/components/user-form.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styles from '../App.module.scss'
+import styles from "../App.module.scss";
 
 export default function SignInForm() {
   const [name, setName] = useState("");
@@ -10,113 +10,189 @@ export default function SignInForm() {
   const [passwordError, setPasswordError] = useState("");
   const [username, setUsername] = useState("");
   const [usernameError, setUsernameError] = useState("");
+  const [fields, setFields] = useState({
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [fieldErrors, setFieldErrors] = useState({
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+  });
 
-  const emailRegex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
+  const emailRegex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+  const usernameRegex = /^[a-z0-9_]+$/;
+  const nameRegex = /^[a-zA-Z ]+$/;
 
-  const usernameRegex = /^[a-z0-9\.\_]{5,15}$/
+  const handleNameChange = (value) => {
+    const error = "Name can only contain characters";
+    if (value.length !== 0) {
+      if (!nameRegex.test(value.trim())) {
+        setNameError(error);
+        setFieldErrors(Object.assign(fieldErrors, { name: error }));
+      }
+    }
+    setName(value);
+    setNameError(null);
+    setFieldErrors(Object.assign(fieldErrors, { name: null }));
+    setFields(Object.assign(fields, { name: value }));
+  };
 
-  const nameRegex = /^[a-zA-Z \.\']{2,30}$/
+  const handleUsernameChange = (value) => {
+    const error = "username can only contain characters and underscore('_')";
+    if (value.length !== 0) {
+      if (!usernameRegex.test(value.trim())) {
+        setUsernameError(error);
+        setFieldErrors(Object.assign(fieldErrors, { username: error }));
+      }
+    }
+    setUsername(value);
+    setUsernameError(null);
+    setFieldErrors(Object.assign(fieldErrors, { username: null }));
+    setFields(Object.assign(fields, { username: value }));
+  };
 
-  const passwordRegex = /[!@#$%^&*(){}[\]<>?/|.:;_-]/
-
-  const handleSubmit = () => {
-    if (name.trim() === "") {
-      setNameError('Name Cannot be empty')
-      return
-    }
-    if (username.trim() === "") {
-      return setNameError('Username Cannot be empty')
-    }
-    if (email.trim() === "") {
-      return setNameError('Email Cannot be empty')
-    }
-    if (password.trim() === "") {
-      return setNameError('Password Cannot be empty')
-    }
-    if (!nameRegex.test(name.trim())) {
-      return setNameError('Invalid Name')
-    }
-    if (!usernameRegex.test(username.trim())) {
-      return setUsernameError('Invalid Username')
-    }
-    if (!emailRegex.test(email.trim())) {
-      return setEmailError('Invalid Email')
-    }
-    if (!passwordRegex.test(password.trim())) {
-      return setPasswordError('Invalid Password')
-    }
-    alert(
-      `The form was submitted with the following data: name: ${name}, username: ${username}, email: ${email}, password: ${password}`
+  const handleEmailChange = (value) => {
+    let atPos = value.indexOf("@");
+    let dotPos = value.indexOf(".");
+    if (value.indexOf("@") === -1) {
+      setEmail(value);
+      setFields(Object.assign(fields, { email: value }));
+      setEmailError("@ is missing");
+      return setFieldErrors(
+        Object.assign(fieldErrors, { email: "@ is missing" })
       );
+    } else if (atPos > dotPos) {
+      setEmail(value);
+      setFields(Object.assign(fields, { email: value }));
+      setEmailError(". is missing");
+      return setFieldErrors(
+        Object.assign(fieldErrors, { email: ". is missing" })
+      );
+    } else if (dotPos > value.length - 3) {
+      setEmail(value);
+      setFields(Object.assign(fields, { email: value }));
+      setEmailError("Extension is missing");
+      return setFieldErrors(
+        Object.assign(fieldErrors, { email: "Extension is missing" })
+      );
+    } else if (!emailRegex.test(value.trim())) {
+      setEmail(value);
+      setFields(Object.assign(fields, { email: value }));
+      setEmailError("Invalid Email");
+      return setFieldErrors(
+        Object.assign(fieldErrors, { email: "Invalid Email" })
+      );
+    } else {
+      setEmail(value);
+      setEmailError(null);
+      setFieldErrors(Object.assign(fieldErrors, { email: null }));
+      setFields(Object.assign(fields, { email: value }));
+    }
+  };
+
+  const handlePasswordChange = (value) => {
+    setPassword(value);
+    setPasswordError(null);
+    setFieldErrors(Object.assign(fieldErrors, { password: null }));
+    setFields(Object.assign(fields, { password: value }));
+  };
+
+  const checkEmpty = async (key) => {
+    if (fields[key].length === 0) {
+      const errorObj = { ...fieldErrors, [key]: `${key} cannot be empty` };
+      console.log(key, errorObj);
+      return setFieldErrors(errorObj);
+    }
+  };
+
+  const noError = (fields) => {
+    let result = Object.values(fields).every((field) => field === null);
+    return result;
+  };
+
+  const handleSubmit = async () => {
+    Object.keys(fields).map((field) => checkEmpty(field));
+    if (noError(fieldErrors)) {
+      alert(
+        `The form was submitted with the following data: name: ${name}, username: ${username}, email: ${email}, password: ${password}`
+      );
+    }
   };
   return (
     <div className={styles.formCenter}>
       <div className={styles.formTitle}>
-        <h4
-          className={styles.formTitleLink}
-        >
-          Sign up here!
-        </h4>
+        <h4 className={styles.formTitleLink}>Fill up here!</h4>
       </div>
       <div className={styles.formFields}>
         <div className={styles.formField}>
-          <label className={styles.formFieldLabel}>
-            Name
-          </label>
+          <label className={styles.formFieldLabel}>Name</label>
           <input
             type="text"
             className={styles.formFieldInput}
             placeholder="Enter your name"
+            name={"name"}
             value={name}
-            onChange={(e) =>setName(e.target.value)}
+            onChange={(e) => handleNameChange(e.target.value)}
           />
-          <span className={styles.formFieldError}>{nameError}</span>
+          <span className={styles.formFieldError}>
+            {fieldErrors.name || nameError}
+          </span>
         </div>
 
         <div className={styles.formField}>
-          <label className={styles.formFieldLabel}>
-            Username
-          </label>
+          <label className={styles.formFieldLabel}>Username</label>
           <input
             type="text"
             className={styles.formFieldInput}
             placeholder="Enter your username"
+            name={"username"}
             value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            onChange={(e) => handleUsernameChange(e.target.value)}
           />
-          <span className={styles.formFieldError}>{usernameError}</span>
+          <span className={styles.formFieldError}>
+            {fieldErrors.username || usernameError}
+          </span>
         </div>
 
         <div className={styles.formField}>
-          <label className={styles.formFieldLabel}>
-            E-Mail Address
-          </label>
+          <label className={styles.formFieldLabel}>E-Mail Address</label>
           <input
             type="email"
             className={styles.formFieldInput}
             placeholder="Enter your email"
+            name={"email"}
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => handleEmailChange(e.target.value)}
           />
-          <span className={styles.formFieldError}>{emailError}</span>
+          <span className={styles.formFieldError}>
+            {fieldErrors.email || emailError}
+          </span>
         </div>
 
         <div className={styles.formField}>
-          <label className={styles.formFieldLabel}>
-            Password
-          </label>
+          <label className={styles.formFieldLabel}>Password</label>
           <input
             type="password"
             className={styles.formFieldInput}
             placeholder="Enter your password"
+            name={"password"}
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => handlePasswordChange(e.target.value)}
           />
-          <span className={styles.formFieldError}>{passwordError}</span>
+          <span className={styles.formFieldError}>
+            {fieldErrors.password || passwordError}
+          </span>
         </div>
 
         <div className={styles.formField}>
-          <button onClick={() => handleSubmit()} className={styles.formFieldButton}>
+          <button
+            onClick={() => handleSubmit()}
+            className={styles.formFieldButton}
+          >
             Validate
           </button>
         </div>
